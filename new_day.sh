c2635d457e8117ae1b4ac78c57880336f4f16561
Marcin <marcin.konow@lczyk.xyz>
2023-12-08 18:37:12 +0000

    updated new_day script

--- new_day.sh
+++ new_day.sh
@@ -1,26 +1,33 @@
 set -e
-DAY=$1
+XX=$1
 
-# if DAY is not a number, exit
-if ! [[ $DAY =~ ^[0-9]+$ ]]; then
+# if XX is not a number, exit
+if ! [[ $XX =~ ^[0-9]+$ ]]; then
     echo "Usage: $0 <day>"
     exit 1
 fi
 
 
-[ ${#DAY} -eq 1 ] && DAY="0$DAY"
-[ $DAY -gt 25 ] && echo "Day must be between 1 and 25" && exit 1
-[ $DAY -lt 1 ] && echo "Day must be between 1 and 25" && exit 1
+[ ${#XX} -eq 1 ] && XX="0$XX"
+[ $XX -gt 25 ] && echo "Day must be between 1 and 25" && exit 1
+[ $XX -lt 1 ] && echo "Day must be between 1 and 25" && exit 1
 
-DAY_FOLDER="day$DAY"
+DAYXX="day$XX"
 
-[ -d $DAY_FOLDER ] && echo "Folder for day $DAY already exists" && exit 1
+[ -d $DAYXX ] && echo "Folder for day $XX already exists" && exit 1
 
-mkdir $DAY_FOLDER
-cp day00/day00_1.go $DAY_FOLDER/$DAY_FOLDER\_1.go
-cp day00/day00_2.go $DAY_FOLDER/$DAY_FOLDER\_2.go
+mkdir $DAYXX
+cp day00/day00_1.go $DAYXX/$DAYXX\_1.go
+cp day00/day00_2.go $DAYXX/$DAYXX\_2.go
 
-touch data/full/$DAY_FOLDER.txt
-touch data/test/$DAY_FOLDER.txt
+# The first line of each of those files says 'package day00'
+# We need to change that to 'package dayXX'
 
+sed -i '' "s/day00/$DAYXX/g" $DAYXX/$DAYXX\_1.go
+sed -i '' "s/day00/$DAYXX/g" $DAYXX/$DAYXX\_2.go
 
+touch data/full/$DAYXX.txt
+touch data/test/$DAYXX.txt
+
+echo "data for $DAYXX" > data/full/$DAYXX.txt
+echo "data for $DAYXX" > data/test/$DAYXX.txt
