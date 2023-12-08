613699697f080da1b8442e4331a83f60ec536724
Marcin <marcin.konow@lczyk.xyz>
2023-12-08 18:39:47 +0000

    start day 4

---
+++ day04/day04_2.go
@@ -0,0 +1,12 @@
+package day04
+
+import "fmt"
+
+func Main_2(lines []string) (n int, err error) {
+	fmt.Println("Hello from Main_2")
+	fmt.Printf("Got %d lines\n", len(lines))
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+	return 0, nil
+}
