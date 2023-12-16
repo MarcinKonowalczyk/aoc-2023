5ae8380e88b550fbb467be60be641967d3d874a9
Marcin <marcin.konow@lczyk.xyz>
2023-12-16 21:14:11 +0000

    day 06 done

--- day06/day06_1.go
+++ day06/day06_1.go
@@ -3,6 +3,7 @@ package day06
 import (
 	"aoc2023/utils"
 	"fmt"
+	"math"
 	"regexp"
 )
 
@@ -23,7 +24,19 @@ func main_1(lines []string) (n int, err error) {
 	}
 	fmt.Printf("times: %v\n", times)
 	fmt.Printf("distances: %v\n", distances)
-	return 0, nil
+
+	prod_N_times := 1
+	for i := range times {
+		min_time, max_time, err := calcHoldTimeRange(times[i], distances[i])
+		if err != nil {
+			return -1, err
+		}
+		N_times := max_time - min_time + 1
+		fmt.Printf("min_time: %d, max_time: %d, N_times: %d\n", min_time, max_time, N_times)
+		prod_N_times *= N_times
+	}
+
+	return prod_N_times, nil
 }
 
 var TIMES_LINE_RE = regexp.MustCompile(`Time:(?P<times> *(\d+ *)+)`)
@@ -53,4 +66,49 @@ func parseLines(lines []string) (times []int, distances []int, err error) {
 		return nil, nil, err
 	}
 	return times, distances, nil
+}
+
+// t*n - n^2 = d
+// (-d) + (t)*n + (-1)*n^2 = 0
+// (-1)*n^2 + (t)*n + (-d) = 0
+// det = t**2 - 4*d
+// 0.5 * (t +/- sqrt(det))
+// t/2 +/- sqrt(det)/2
+
+func calcHoldTimeRange(time int, distance int) (int, int, error) {
+	int_determinant := time*time - 4*distance
+
+	if int_determinant <= 0 {
+		return -1, -1, fmt.Errorf("invalid determinant")
+	}
+
+	var min_time int
+	var max_time int
+
+	int_det_sqrt, err := perfectSquareRoot(int_determinant)
+
+	if err == nil {
+		// determinant is a perfect square
+		min_time = (time-int_det_sqrt)/2 + 1
+		max_time = (time+int_det_sqrt)/2 - 1
+	} else {
+		float_determinant := float64(int_determinant)
+		float_time := float64(time)
+		float_det_sqrt := math.Sqrt(float_determinant)
+		min_time = int((float_time-float_det_sqrt)/2) + 1
+		max_time = int((float_time + float_det_sqrt) / 2)
+	}
+
+	return min_time, max_time, nil
+}
+
+// Naive implementation because it still relies on floating point arithmetic,
+// but it should be good enough for this problem.
+func perfectSquareRoot(n int) (int, error) {
+	sqrt := math.Sqrt(float64(n))
+	if sqrt == math.Floor(sqrt) {
+		return int(sqrt), nil
+	} else {
+		return -1, fmt.Errorf("not a perfect square")
+	}
 }
--- day06/day06_2.go
+++ day06/day06_2.go
@@ -1,13 +1,65 @@
 package day06
 
-import "fmt"
+import (
+	"aoc2023/utils"
+	"fmt"
+	"strings"
+)
 
 func main_2(lines []string) (n int, err error) {
-	times, distances, err := parseLines(lines)
+	time, distance, err := parseLinesRemoveSpaces(lines)
 	if err != nil {
 		return -1, err
 	}
-	fmt.Printf("times: %v\n", times)
-	fmt.Printf("distances: %v\n", distances)
-	return 0, nil
+	fmt.Printf("time: %v\n", time)
+	fmt.Printf("distance: %v\n", distance)
+
+	min_time, max_time, err := calcHoldTimeRange(time, distance)
+	if err != nil {
+		return -1, err
+	}
+	N_times := max_time - min_time + 1
+
+	return N_times, nil
+}
+
+func parseTimesLineRemoveSpaces(line string) (time int, err error) {
+	matches := utils.GetNamedSubexpsCompiledRe(TIMES_LINE_RE, line)
+	times_without_spaces := strings.ReplaceAll(matches["times"], " ", "")
+	numbers, err := utils.StringOfNumbersToNumbers(times_without_spaces)
+	if err != nil {
+		return -1, err
+	}
+	if len(numbers) != 1 {
+		return -1, fmt.Errorf("invalid input")
+	}
+	return numbers[0], nil
+}
+
+func parseDistancesLineRemoveSpaces(line string) (distance int, err error) {
+	matches := utils.GetNamedSubexpsCompiledRe(DISTANCES_LINE_RE, line)
+	distances_without_spaces := strings.ReplaceAll(matches["distances"], " ", "")
+	numbers, err := utils.StringOfNumbersToNumbers(distances_without_spaces)
+	if err != nil {
+		return -1, err
+	}
+	if len(numbers) != 1 {
+		return -1, fmt.Errorf("invalid input")
+	}
+	return numbers[0], nil
+}
+
+func parseLinesRemoveSpaces(lines []string) (time int, distance int, err error) {
+	if len(lines) != 2 {
+		return -1, -1, fmt.Errorf("invalid input")
+	}
+	time, err = parseTimesLineRemoveSpaces(lines[0])
+	if err != nil {
+		return -1, -1, err
+	}
+	distance, err = parseDistancesLineRemoveSpaces(lines[1])
+	if err != nil {
+		return -1, -1, err
+	}
+	return time, distance, nil
 }
