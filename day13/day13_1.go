0ae820cedd14e63a6edb1739a0530a78dacd3219
Marcin <marcin.konow@lczyk.xyz>
2024-07-14 11:13:27 +0100

    parse map

--- day13/day13_1.go
+++ day13/day13_1.go
@@ -1,6 +1,8 @@
 package day13
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Main(part int, lines []string) (n int, err error) {
 	if part == 1 {
@@ -13,10 +15,70 @@ func Main(part int, lines []string) (n int, err error) {
 }
 
 func main_1(lines []string) (n int, err error) {
-	fmt.Println("Hello from main_1")
-	fmt.Printf("Got %d lines\n", len(lines))
-	for _, line := range lines {
-		fmt.Println(line)
+	maps, err := parseLines(lines)
+	if err != nil {
+		return -1, err
+	}
+	fmt.Println("Found maps:", len(maps))
+	for _, m := range maps {
+		fmt.Println(m)
 	}
 	return 0, nil
+}
+
+type Map [][]bool
+
+func (m Map) nRows() int {
+	return len(m)
+}
+
+func (m Map) nCols() int {
+	if len(m) == 0 {
+		return 0
+	}
+	return len(m[0])
+}
+
+func (m Map) String() string {
+	s := "Map:\n"
+	for _, row := range m {
+		for _, cell := range row {
+			if cell {
+				s += "#"
+			} else {
+				s += "."
+			}
+		}
+		s += "\n"
+	}
+	return s
+}
+
+func parseLines(lines []string) ([]Map, error) {
+	var maps []Map = make([]Map, 0)
+	var m Map = make(Map, 0)
+	for _, line := range lines {
+		if line == "" {
+			if m.nRows() > 0 {
+				maps = append(maps, m)
+				m = make(Map, 0)
+			}
+		} else {
+			row := make([]bool, 0)
+			for _, c := range line {
+				if c == '.' {
+					row = append(row, false)
+				} else if c == '#' {
+					row = append(row, true)
+				} else {
+					return nil, fmt.Errorf("invalid character: %c", c)
+				}
+			}
+			m = append(m, row)
+		}
+	}
+	if m.nRows() > 0 {
+		maps = append(maps, m)
+	}
+	return maps, nil
 }
