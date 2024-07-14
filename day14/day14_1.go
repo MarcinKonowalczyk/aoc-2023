0df3c22e949258dc3a70498b690cbd8d0f73866c
Marcin <marcin.konow@lczyk.xyz>
2024-07-14 22:50:42 +0100

    works but v slow

--- day14/day14_1.go
+++ day14/day14_1.go
@@ -13,10 +13,113 @@ func Main(part int, lines []string) (n int, err error) {
 }
 
 func main_1(lines []string) (n int, err error) {
-	fmt.Println("Hello from main_1")
-	fmt.Printf("Got %d lines\n", len(lines))
-	for _, line := range lines {
-		fmt.Println(line)
+	grid := parseLines(lines)
+	slideNorth(&grid)
+	weight := calcNorthWeight(&grid)
+	return weight, nil
+}
+
+type Rock int
+
+const (
+	EMPTY Rock = iota
+	ROUND
+	SQUARE
+)
+
+func (r Rock) String() string {
+	switch r {
+	case EMPTY:
+		return "."
+	case ROUND:
+		return "O"
+	case SQUARE:
+		return "#"
 	}
-	return 0, nil
+	return "?"
+}
+
+type Grid [][]Rock
+
+func (g Grid) String() string {
+	s := ""
+	for i, row := range g {
+		for _, rock := range row {
+			s += rock.String()
+		}
+		if i < len(g)-1 {
+			s += "\n"
+		}
+	}
+	return s
+}
+func parseLines(lines []string) Grid {
+	grid := make(Grid, len(lines))
+	for i, line := range lines {
+		grid[i] = make([]Rock, len(line))
+		for j, c := range line {
+			switch c {
+			case '.':
+				grid[i][j] = EMPTY
+			case 'O':
+				grid[i][j] = ROUND
+			case '#':
+				grid[i][j] = SQUARE
+			default:
+				panic("invalid character")
+			}
+		}
+	}
+	return grid
+}
+
+// Slide all the round rocks in the grid to the north
+func slideNorth(g *Grid) {
+	n_rows := len(*g)
+	for i := 0; i < n_rows; i++ {
+		// Slide the ith row to the north as much as possible
+		slideRowNorth(g, i)
+	}
+}
+
+func slideRowNorth(g *Grid, row int) {
+	n_cols := len((*g)[0])
+	for col := 0; col < n_cols; col++ {
+		// Slide the ith column to the north as much as possible
+		slideRowColNorth(g, row, col)
+	}
+}
+
+func slideRowColNorth(g *Grid, row, col int) {
+	if (*g)[row][col] != ROUND {
+		// The rock is not round, so it cannot be slid
+		return
+	}
+	if row == 0 {
+		// The rock is already at the top of the grid so cannot be slid
+		return
+	}
+	rock_above := (*g)[row-1][col]
+	if rock_above == EMPTY {
+		// The rock can be slid to the north
+		(*g)[row-1][col] = ROUND
+		(*g)[row][col] = EMPTY
+		// Call the function recursively to keep sliding the rock to the north
+		slideRowColNorth(g, row-1, col)
+	}
+}
+
+// ======================================================================
+
+func calcNorthWeight(g *Grid) int {
+	weight := 0
+	n_rows := len(*g)
+	for i := 0; i < n_rows; i++ {
+		for j := 0; j < len((*g)[0]); j++ {
+			if (*g)[i][j] == ROUND {
+				weight += (n_rows - i)
+			}
+		}
+	}
+	return weight
 }
--- day14/day14_2.go
+++ day14/day14_2.go
@@ -2,11 +2,64 @@ package day14
 
 import "fmt"
 
+const N = 1000000000
+
 func main_2(lines []string) (n int, err error) {
-	fmt.Println("Hello from main_2")
-	fmt.Printf("Got %d lines\n", len(lines))
-	for _, line := range lines {
-		fmt.Println(line)
+	grid := parseLines(lines)
+	spinCycle(&grid)
+	fmt.Println("After 1 spin cycle:")
+	fmt.Println(grid)
+	spinCycle(&grid)
+	fmt.Println("After 2 spin cycles:")
+	fmt.Println(grid)
+	spinCycle(&grid)
+	fmt.Println("After 3 spin cycles:")
+	fmt.Println(grid)
+
+	// for i := 0; i < N; i++ {
+	// 	if i%(N/10000) == 0 {
+	// 		done_percent := float64(i) / N
+	// 		fmt.Printf("%.4f%%\n", done_percent)
+	// 	}
+	// 	spinCycle(&grid)
+	// }
+	weight := calcNorthWeight(&grid)
+	return weight, nil
+}
+
+type Direction int
+
+const (
+	NORTH Direction = iota
+	EAST
+	SOUTH
+	WEST
+)
+
+func rotateClockwise(g *Grid) {
+	n_rows := len(*g)
+	n_cols := len((*g)[0])
+	var temp Rock // for swapping
+	for i := 0; i < n_rows/2; i++ {
+		for j := 0; j < n_cols/2; j++ {
+			ii := n_rows - i - 1 // i from the bottom
+			jj := n_cols - j - 1 // j from the right
+			temp = (*g)[i][j]
+			(*g)[i][j] = (*g)[jj][i]
+			(*g)[jj][i] = (*g)[ii][jj]
+			(*g)[ii][jj] = (*g)[j][ii]
+			(*g)[j][ii] = temp
+		}
 	}
-	return 0, nil
+}
+
+func spinCycle(g *Grid) {
+	slideNorth(g)      // actual north
+	rotateClockwise(g) // north is now east, so slideNorth is slideWest
+	slideNorth(g)      // west
+	rotateClockwise(g) // ...
+	slideNorth(g)      // south
+	rotateClockwise(g)
+	slideNorth(g)      // east
+	rotateClockwise(g) // back to the original orientation
 }
