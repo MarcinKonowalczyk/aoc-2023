5379233836ee6f5abed45e717259ac486ca2bf1e
Marcin <marcin.konow@lczyk.xyz>
2024-07-27 15:12:43 +0100

    djkstra works

--- dijkstra/dijkstra_test.go
+++ dijkstra/dijkstra_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestDjkstra_SimpleGraph1(t *testing.T) {
+func TestDjkstra_ABC_Line(t *testing.T) {
 	g := Graph{}
 	vA := NewVertex()
 	vB := NewVertex()
@@ -21,6 +21,51 @@ func TestDjkstra_SimpleGraph1(t *testing.T) {
 	utils.AssertEqualArrays(t, path, []*Vertex{vA, vB, vC})
 }
 
-func TestDjkstra_SimpleGraph2(t *testing.T) {
+func TestDjkstra_ABC_Cycle(t *testing.T) {
+	g := Graph{}
+	vA := NewVertex()
+	vB := NewVertex()
+	vC := NewVertex()
+	g.AddVertex(vA)
+	g.AddVertex(vB)
+	g.AddVertex(vC)
+	g.AddEdge(vA, vB, 1)
+	g.AddEdge(vA, vC, 2)
+	g.AddEdge(vB, vC, 3)
 
+	path, distande := ShortestPath(&g, vA, vC)
+	utils.AssertEqual(t, distande, 2)
+	utils.AssertEqualArrays(t, path, []*Vertex{vA, vC})
+}
+
+func TestDjkstra_LargerCase(t *testing.T) {
+	// This is the graph from the wikipedia page on Dijkstra's algorithm
+	g := Graph{}
+	vA := NewVertex()
+	vB := NewVertex()
+	vC := NewVertex()
+	vD := NewVertex()
+	vE := NewVertex()
+	vF := NewVertex()
+
+	g.AddVertex(vA)
+	g.AddVertex(vB)
+	g.AddVertex(vC)
+	g.AddVertex(vD)
+	g.AddVertex(vE)
+	g.AddVertex(vF)
+
+	g.AddEdge(vA, vB, 7)
+	g.AddEdge(vA, vC, 9)
+	g.AddEdge(vA, vD, 14)
+	g.AddEdge(vB, vC, 10)
+	g.AddEdge(vB, vF, 15)
+	g.AddEdge(vC, vD, 2)
+	g.AddEdge(vC, vF, 11)
+	g.AddEdge(vD, vE, 9)
+	g.AddEdge(vE, vF, 6)
+
+	path, distande := ShortestPath(&g, vA, vE)
+	utils.AssertEqual(t, distande, 20)
+	utils.AssertEqualArrays(t, path, []*Vertex{vA, vC, vD, vE})
 }
--- dijkstra/graph_elements.go
+++ dijkstra/graph_elements.go
@@ -21,13 +21,6 @@ func NewVertex() *Vertex {
 	return &v
 }
 
-func (n *Vertex) String() string {
-	return fmt.Sprintf("V%d", n.index)
-}
-
-var edge_counter int  // Global counter for number of edges initialized
-var all_edges []*Edge // List of all edges
-
 type Edge struct {
 	Vertex *Vertex // The vertex at the end of the edge
 	Weight int
