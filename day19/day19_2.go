a0e7940d3c9f5b8dc78e711f3801064d78a5172b
Marcin <marcin.konow@lczyk.xyz>
2024-09-08 14:50:41 +0100

    part 2 start

--- day19/day19_2.go
+++ day19/day19_2.go
@@ -1,12 +1,41 @@
 package day19
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main_2(lines []string) (n int, err error) {
-	fmt.Println("Hello from main_2")
-	fmt.Printf("Got %d lines\n", len(lines))
-	for _, line := range lines {
-		fmt.Println(line)
+	workflow_lines, part_lines, err := cutAtLine(lines, "")
+	if err != nil {
+		return 0, err
 	}
+	var workflows []Workflow = make([]Workflow, len(workflow_lines))
+	for i, line := range workflow_lines {
+		workflow, err := ParseWorkflowLine(line)
+		if err != nil {
+			return -1, err
+		}
+		workflows[i] = workflow
+	}
+
+	var parts []Part = make([]Part, len(part_lines))
+	for i, line := range part_lines {
+		part, err := ParsePartLine(line)
+		if err != nil {
+			return -1, err
+		}
+		parts[i] = part
+	}
+
+	fmt.Println("Workflows:")
+	for _, workflow := range workflows {
+		fmt.Println(" ", workflow)
+	}
+
+	fmt.Println("Parts:")
+	for _, part := range parts {
+		fmt.Println(" ", part)
+	}
+
 	return 0, nil
 }
